dc5d36518207571f484c9841e69534d55f8f4563
Kazi Alfaiz <alfaizkazi1@gmail.com>
2025-10-27 18:40:34 +0530

    1st day to learn spring

---
+++ SpringDemo/src/main/java/Demo/Doctor.java
@@ -0,0 +1,11 @@
+package Demo;
+
+import javax.crypto.spec.PSource;
+
+public class Doctor {
+
+    public void assist(){
+        System.out.println(" Doctor is assisting");
+    }
+
+}
---
+++ SpringDemo/src/main/java/Demo/main.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class main {
+    public static void main(String[] args) {
+        Doctor doctor= new Doctor();
+        doctor.assist();
+
+
+    }
+}
